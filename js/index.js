2c42fa11bb429a6e768f8261d1fcbccb71b27401
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-11 12:59:18 -0500

    I was having trouple with git hub and kep getting errors when i tried to push but i have fixed them now!

--- js/index.js
+++ js/index.js
@@ -11,3 +11,64 @@ function clickEventHandler(event) {
   document.body.addEventListener('click', clickEventHandler)
   document.addEventListener('click', clickEventHandler)
   window.addEventListener('click', clickEventHandler)
+
+
+  const body = document.querySelector("body");
+
+//1
+
+body.addEventListener('click', () => {
+  body.style.backgroundColor="papayawhip";
+  console.log("end")
+});
+
+//2
+
+const logoChange = document.querySelector(".logo-heading");
+console.log(logoChange);
+logoChange.addEventListener("mouseenter", () => {
+logoChange.style.transform = "scale(1.2)";
+logoChange.style.transition = "all 0.3s";
+})
+
+//3
+
+const picChange = document.querySelector(".intro")
+picChange.addEventListener("dblclick", () => {
+  picChange.style.transform = "scale(2)";
+})
+
+//4
+
+const yolo = document.querySelector(".footer")
+yolo.addEventListener("mousedown", () => {
+  yolo.style.color = "blue";
+})
+
+//5
+
+const yolo1 = document.querySelector(".destination")
+yolo1.addEventListener("mouseover", () => {
+  yolo1.style.color = "red";
+})
+
+//6
+
+const yolo2 = document.querySelector(".intro")
+yolo2.addEventListener("mousemove", () => {
+  yolo2.style.color = "green";
+})
+
+//7
+
+const yolo3 = document.querySelector(".content-section")
+yolo3.addEventListener("mousewheel", () => {
+  yolo3.style.transform = "scale(3)";
+})
+
+//8
+
+const yolo4 = document.querySelector("#homeLink")
+yolo4.addEventListener("mouseout", () => {
+  yolo4.style.color= "purple";
+})
